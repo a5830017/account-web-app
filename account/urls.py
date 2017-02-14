72b1a6a5c85e187700551dc61bb5fc78be306513
aa5830017 <a5801012630017@gmail.com>
2017-02-14 07:12:32 +0000

    add template and fake views

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'), #first page -- index
     url(r'^accountsetting/$', views.AccSetting, name='accountsetting'), #account setting -- access from index
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), #detail page -- access from index
+    url(r'^(?P<account_id>[0-9]+)/addlist/$', views.addlist, name='addlist'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+from django.template import loader
 
 from .models import Account, List
 
@@ -17,7 +19,24 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Account
     template_name = 'account/detail.html'
+'''
+    def add_list(self):
+        try:
+            addlist = request.POST['addlist']
+            moneyget = request.POST['moneyget']
+            moneypay = request.POST['moneypay']
+        except:
+            addlist = ""
+            moneyget = ""
+            moneypay = ""
+        else:
+            q = Account(list_text=addlist, get_money=moneyget, pay_money=moneypay, pub_date=timezone.now())
+        return q.save()'''
 
 def AccSetting(request): #add account
     return render(request, "account/accsetting.html", '')
+
+def addlist(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
+    return render(request, "account/addlist.html", {'account': account },)
 
